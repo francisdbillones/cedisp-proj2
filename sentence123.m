7c887f0356abe12fd9cca1a8f66a6d96d1f5837a
kent-lopez9 <kent_lopez@dlsu.edu.ph>
2026-03-29 20:37:43 +0800

    Add files via upload

---
+++ sentence123.m
@@ -0,0 +1,114 @@
+clear; clc; close all;
+
+[x, Fs] = audioread('PG02.wav');
+
+% plot audio so we can see the indiv syllables
+t = (0:length(x)-1) / Fs;
+figure;
+plot(t, x);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Full audio signal');
+
+ma_syllable = x(t >= 0.46 & t <= 0.72);
+gan_syllable = x(t >= 1.58 & t <= 2.0);
+dang_syllable = x(t >= 2.71 & t <= 3.18);
+ha_syllable = x(t >= 3.85 & t <= 4.1);
+pon_syllable = x(t >= 4.88 & t <= 5.2);
+ga_syllable = x(t >= 6.90 & t <= 7.1);
+bi_syllable = x(t >= 8.00 & t <= 8.33);
+
+% Speed up "Magandang" for greeting (10% faster)
+ma_fast   = resample(ma_syllable, 90, 100);
+gan_fast  = resample(gan_syllable, 90, 100);
+dang_fast = resample(dang_syllable, 90, 100);
+
+% Sentence 1
+pon_fast  = resample(pon_syllable, 90, 100);
+s1_dang_syllable = resample(dang_syllable, round(0.925*100), 100);
+s1_ha_syllable = resample(ha_syllable, round(0.925*100), 100);
+
+sentence = [ma_fast; 
+             gan_fast; 
+             s1_dang_syllable; 
+             s1_ha_syllable; 
+             pon_fast];
+
+figure;
+plot((0:length(sentence)-1) / Fs, sentence);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Good afternoon');
+sound(sentence, Fs);
+
+
+pause(3)
+
+
+% Sentence 2: /Magandang ha'pon?/ 
+% turning 'hapon! to ha'pon?
+
+% reducing the amplitude and duration of ha
+ha_unstressed = 0.6 * ha_syllable(1:floor(length(ha_syllable)*0.7)); 
+
+% gradually increasing pitch for 'pon' 
+pon_syl_ques = [];
+N = length(pon_syllable)
+t = (0:N-1)/Fs;
+for i = 1:60
+    start_idx = floor((i-1)*N/60) + 1;
+    end_idx   = floor(i*N/60);
+    segment = pon_syllable(start_idx:end_idx);
+    
+    factor = 1 - i*0.005;
+    seg_resampled = resample(segment, round(factor*100), 100);
+
+    %adding the fade effects to remove cracking/hissing sounds
+    fade_in = linspace(0,1,length(seg_resampled));
+    fade_out = linspace(1,0,length(seg_resampled));
+    
+    seg_resampled = 3.*seg_resampled .* fade_in' .* fade_out';  
+    pon_syl_ques = [pon_syl_ques; seg_resampled];
+end
+
+
+out_sentence = [
+             ma_syllable
+             gan_syllable
+             dang_syllable
+             ha_unstressed; 
+             pon_syl_ques];
+
+figure;
+plot((0:length(out_sentence)-1) / Fs, out_sentence);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Magandang hapon? (beautiful japanese?)');
+sound(out_sentence, Fs);
+
+pause(3)
+
+% Sentence 3: /Magandang ga'bi!/
+ga_syllable = resample(ga_syllable, 90, 100);
+% add stress and falling Intonation 
+% divide the syllable /bi/
+mid = round(length(bi_syllable) / 2);
+bi_part1 = bi_syllable(1:mid);
+bi_part2 = bi_syllable(mid+1:end);
+% create a falling intonation, resample the second half to be 10% slower (lowers pitch)
+bi_part2_falling = resample(bi_part2, 110, 100);
+% Combine and apply a volume decay envelope for a natural "tail"
+s_bi_final = [bi_part1; bi_part2_falling];
+env = [ones(length(bi_part1), 1); linspace(1, 0.5, length(bi_part2_falling))'];
+s_bi_final = s_bi_final .* env * 2.5; % 2.5x Gain for Exclamatory Stress (!)
+
+%combine for sentence 3
+sentence_3 = [ma_fast; gan_fast; dang_fast; ga_syllable; s_bi_final];
+
+figure;
+plot((0:length(sentence_3)-1)/Fs, sentence_3);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title("/Magandang ga'bi!/ (Good evening!)");
+sound(sentence_3, Fs);
+audiowrite("sentence3.wav", sentence_3, Fs);
